5a34f880c63c93f8d1ab8276df6b9356879b8ed8
Matias Brond <matibrond@gmail.com>
2019-05-10 12:41:54 -0300

    deleted person

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -56,60 +56,7 @@ public class Application {
         restHighLevelClient = null;
     }
 
-    private static Person insertPerson(Person person){
-        person.setPersonId(UUID.randomUUID().toString());
-        Map<String, Object> dataMap = new HashMap<String, Object>();
-        dataMap.put("personId", person.getPersonId());
-        dataMap.put("name", person.getName());
-        IndexRequest indexRequest = new IndexRequest(INDEX, TYPE, person.getPersonId())
-                .source(dataMap);
-        try {
-            IndexResponse response = restHighLevelClient.index(indexRequest,RequestOptions.DEFAULT);
-        } catch(ElasticsearchException e) {
-            e.getDetailedMessage();
-        } catch (IOException ex){
-            ex.getLocalizedMessage();
-        }
-        return person;
-    }
-
-    private static Person getPersonById(String id){
-        GetRequest getPersonRequest = new GetRequest(INDEX, TYPE, id);
-        GetResponse getResponse = null;
-        try {
-            getResponse = restHighLevelClient.get(getPersonRequest, RequestOptions.DEFAULT);
-        } catch (IOException e){
-            e.getLocalizedMessage();
-        }
-        return getResponse != null ?
-                objectMapper.convertValue(getResponse.getSourceAsMap(), Person.class) : null;
-    }
-
-    private static Person updatePersonById(String id, Person person){
-        UpdateRequest updateRequest = new UpdateRequest(INDEX, TYPE, id)
-                .fetchSource(true);    // Fetch Object after its update
-        try {
-            String personJson = objectMapper.writeValueAsString(person);
-            updateRequest.doc(personJson, XContentType.JSON);
-            UpdateResponse updateResponse = restHighLevelClient.update(updateRequest,RequestOptions.DEFAULT);
-            return objectMapper.convertValue(updateResponse.getGetResult().sourceAsMap(), Person.class);
-        }catch (JsonProcessingException e){
-            e.getMessage();
-        } catch (IOException e){
-            e.getLocalizedMessage();
-        }
-        System.out.println("Unable to update person");
-        return null;
-    }
-
-    private static void deletePersonById(String id) {
-        DeleteRequest deleteRequest = new DeleteRequest(INDEX, TYPE, id);
-        try {
-            DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest,RequestOptions.DEFAULT);
-        } catch (IOException e){
-            e.getLocalizedMessage();
-        }
-    }
+    
 
     public static void main(String[] args) throws IOException {
 
